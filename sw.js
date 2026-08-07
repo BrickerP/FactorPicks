3b46b12f6f4c73bf9066ed02df7d8e1805d948e2
BrickerP <BrickerP@users.noreply.github.com>
2026-08-07 07:25:35 +0000

    deploy: 8cd9297b3273b816fc70539d90882e4350f49d4b

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-361f540e2ba2ac8fab1b.js"
+    "url": "webpack-runtime-ac8c7964f20b3a9e587c.js"
   },
   {
     "url": "framework-cf6ff516a1978576dfe4.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "409083fee8162d95ec3c34d38477b070"
+    "revision": "0ab51a175b60006bea4a5355548f1a4e"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-49be599b1ad8fa2a8465.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "eea6d7f011f8b94c3c8a7be18189bc90"
+    "revision": "6a11324e47746e455683fa9715686d1e"
   },
   {
     "url": "polyfill-74d6d6f8f4507f0091d0.js"
